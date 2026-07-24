ff64ade7f22c5a26c6fd69cb2e50e1714c205f7b
mohit sharma <mohitttt009@gmail.com>
2026-07-24 21:31:22 +0530

    Refactor CSS for improved styling and responsiveness; update navigation links in customer.php

--- admin/customer.php
+++ admin/customer.php
@@ -50,7 +50,7 @@ $query = mysqli_query($connection, "SELECT * FROM customer ORDER BY id DESC");
         <ul>
 
             <li>
-                <a href="index.php">
+                <a href="dash.php">
                     <i class="fa-solid fa-house"></i>
                     Dashboard
                 </a>
@@ -64,7 +64,7 @@ $query = mysqli_query($connection, "SELECT * FROM customer ORDER BY id DESC");
             </li>
 
             <li>
-                <a href="products.php">
+                <a href="product.php">
                     <i class="fa-solid fa-box"></i>
                     Products
                 </a>
